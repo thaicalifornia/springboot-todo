dd1a63338c6f8a9bda36839d41d60a22b983f046
chadusa <chet_usa@hotmail.com>
2019-10-21 10:55:22 -0700

    completed connect JPA database

--- src/main/java/com/thaicalifornia/rest/webservice/restfulwebservices/todo/Todo.java
+++ src/main/java/com/thaicalifornia/rest/webservice/restfulwebservices/todo/Todo.java
@@ -2,8 +2,17 @@ package com.thaicalifornia.rest.webservice.restfulwebservices.todo;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
-	private long id;
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
 	private String username;
 	private String description;
 	private Date targetDate;
@@ -21,10 +30,10 @@ public class Todo {
 		this.targetDate = targetDate;
 		this.isDone = isDone;
 	}
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getUsername() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date, is_done)
+values(1001, 'michele', 'learn java javascripts', sysdate(), false);
+
+insert into todo(id, username,description,target_date, is_done)
+values(1002, 'michele', 'learn datastructure, algorythm', sysdate(), false);
+
+insert into todo(id, username,description,target_date, is_done)
+values(1003, 'michele', 'go shopping with my wife', sysdate(), false);
